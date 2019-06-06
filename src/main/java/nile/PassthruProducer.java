56e0e14e81a2dbd74f41e20d6558a3d81bcd3305
Sean Halliburton <sean.halliburton@nordstrom.com>
2019-06-06 12:47:53 -0700

    first commit

---
+++ src/main/java/nile/PassthruProducer.java
@@ -0,0 +1,18 @@
+package nile;
+
+import org.apache.kafka.clients.producer.*;
+
+public class PassthruProducer implements IProducer {
+
+  private final KafkaProducer<String, String> producer;
+  private final String topic;
+
+  public PassthruProducer(String servers, String topic) {
+    this.producer = new KafkaProducer(IProducer.createConfig(servers));
+    this.topic = topic;
+  }
+
+  public void process(String message) {
+    IProducer.write(this.producer, this.topic, message);
+  }
+}
---
+++ src/main/java/nile/StreamApp.java
@@ -0,0 +1,15 @@
+package nile;
+
+public class StreamApp {
+
+  public static void main(String[] args) {
+    String servers = args[0];
+    String groupId = args[1];
+    String inTopic = args[2];
+    String goodTopic = args[3];
+
+    Consumer consumer = new Consumer(servers, groupId, inTopic);
+    PassthruProducer  producer = new PassthruProducer(servers, goodTopic);
+    consumer.run(producer);
+  }
+}
