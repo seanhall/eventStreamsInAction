a679f9b3bc1f589348ac050cceb8654a7a59f9a3
Sean Halliburton <sean.halliburton@nordstrom.com>
2019-06-09 22:28:51 -0700

    Revert "adding full enrichment stream processor"
    This reverts commit 6abb46dc2693630ae92d49db43236103f65826c2.

--- src/main/java/nile/StreamApp.java
+++ src/main/java/nile/StreamApp.java
@@ -1,22 +1,15 @@
 package nile;
 
-import java.io.*;
-
-import com.maxmind.geoip2.DatabaseReader;
-
 public class StreamApp {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     String servers = args[0];
     String groupId = args[1];
     String inTopic = args[2];
     String goodTopic = args[3];
-    String badTopic = args[4];
-    String maxmindFile = args[5];
 
     Consumer consumer = new Consumer(servers, groupId, inTopic);
-    DatabaseReader maxmind = new DatabaseReader.Builder(new File(maxmindFile)).build();
-    FullProducer producer = new FullProducer(servers, goodTopic, badTopic, maxmind);
+    PassthruProducer  producer = new PassthruProducer(servers, goodTopic);
     consumer.run(producer);
   }
 }
