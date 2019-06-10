6abb46dc2693630ae92d49db43236103f65826c2
Sean Halliburton <sean.halliburton@nordstrom.com>
2019-06-09 22:26:53 -0700

    adding full enrichment stream processor

--- src/main/java/nile/StreamApp.java
+++ src/main/java/nile/StreamApp.java
@@ -1,15 +1,22 @@
 package nile;
 
+import java.io.*;
+
+import com.maxmind.geoip2.DatabaseReader;
+
 public class StreamApp {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     String servers = args[0];
     String groupId = args[1];
     String inTopic = args[2];
     String goodTopic = args[3];
+    String badTopic = args[4];
+    String maxmindFile = args[5];
 
     Consumer consumer = new Consumer(servers, groupId, inTopic);
-    PassthruProducer  producer = new PassthruProducer(servers, goodTopic);
+    DatabaseReader maxmind = new DatabaseReader.Builder(new File(maxmindFile)).build();
+    FullProducer producer = new FullProducer(servers, goodTopic, badTopic, maxmind);
     consumer.run(producer);
   }
 }
